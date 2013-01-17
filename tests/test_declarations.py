25ae0859c2a8341b0c4857a9046c17683385ccae
Chris Lasher <chris.lasher@gmail.com>
2013-01-17 16:29:14 -0500

    Use extracted argument in PostGenerationMethodCall.
    This changeset makes it possible possible to override the default method
    arguments (or "method_args") passed in when instantiating
    PostGenerationMethodCall. Now the user can override the default
    arguments to the method called during post-generation when instantiating
    a factory.
    
    For example, using this UserFactory,
    
        class UserFactory(factory.Factory):
            FACTORY_FOR = User
    
            username = factory.Sequence(lambda n: 'user{0}'.format(n))
            password = factory.PostGenerationMethodCall(
                    'set_password', None, 'defaultpassword')
    
    by default, the user will have a password set to 'defaultpassword', but
    this can be overridden by passing in a new password as a keyword
    argument:
    
        >>> u = UserFactory()
        >>> u.check_password('defaultpassword')
        True
        >>> other_u = UserFactory(password='different')
        >>> other_u.check_password('defaultpassword')
        False
        >>> other_u.check_password('different')
        True
    
    This changeset introduces a testing dependency on the Mock package
    http://pypi.python.org/pypi/mock. While this is a third-party dependency
    in Python 2, it is part of the Python 3 standard library, as unit.mock,
    and so a reasonable dependency to satisfy.

--- tests/test_declarations.py
+++ tests/test_declarations.py
@@ -22,6 +22,8 @@
 
 import datetime
 
+from mock import MagicMock
+
 from factory import declarations
 
 from .compat import unittest
@@ -103,6 +105,51 @@ class PostGenerationDeclarationTestCase(unittest.TestCase):
             {'foo': 13, 'foo__bar': 42, 'blah': 42, 'blah__baz': 1})
         self.assertEqual(extracted, 42)
         self.assertEqual(kwargs, {'baz': 1})
+
+
+class PostGenerationMethodCallTestCase(unittest.TestCase):
+    def setUp(self):
+        self.obj = MagicMock()
+
+    def test_simplest_setup_and_call(self):
+        decl = declarations.PostGenerationMethodCall('method')
+        decl.call(self.obj, False)
+        self.obj.method.assert_called_once_with()
+
+    def test_call_with_method_args(self):
+        decl = declarations.PostGenerationMethodCall(
+                'method', None, 'data')
+        decl.call(self.obj, False)
+        self.obj.method.assert_called_once_with('data')
+
+    def test_call_with_passed_extracted_string(self):
+        decl = declarations.PostGenerationMethodCall(
+                'method', None)
+        decl.call(self.obj, False, 'data')
+        self.obj.method.assert_called_once_with('data')
+
+    def test_call_with_passed_extracted_int(self):
+        decl = declarations.PostGenerationMethodCall('method')
+        decl.call(self.obj, False, 1)
+        self.obj.method.assert_called_once_with(1)
+
+    def test_call_with_passed_extracted_iterable(self):
+        decl = declarations.PostGenerationMethodCall('method')
+        decl.call(self.obj, False, (1, 2, 3))
+        self.obj.method.assert_called_once_with(1, 2, 3)
+
+    def test_call_with_method_kwargs(self):
+        decl = declarations.PostGenerationMethodCall(
+                'method', None, data='data')
+        decl.call(self.obj, False)
+        self.obj.method.assert_called_once_with(data='data')
+
+    def test_call_with_passed_kwargs(self):
+        decl = declarations.PostGenerationMethodCall('method')
+        decl.call(self.obj, False, data='other')
+        self.obj.method.assert_called_once_with(data='other')
+
+
 
 
 class CircularSubFactoryTestCase(unittest.TestCase):
