7a7bfc750605701cb7d82656e918a5b375fbc385
Raphaël Barrois <raphael.barrois@polytechnique.org>
2013-03-28 23:42:32 +0100

    Advertise PyPy support.

--- setup.py
+++ setup.py
@@ -90,6 +90,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Testing',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
