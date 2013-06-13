ac0a08e0a60cdf2b2e299595383c7f737edd6532
minimumserious <commande.romain@gmail.com>
2013-06-13 10:33:56 +0200

    Fixed SQLAlchemy tests on Python32

--- tests/test_alchemy.py
+++ tests/test_alchemy.py
@@ -50,14 +50,14 @@ class StandardFactory(SQLAlchemyModelFactory):
     FACTORY_SESSION = models.session
 
     id = factory.Sequence(lambda n: n)
-    foo = factory.Sequence(lambda n: u'foo%d' % n)
+    foo = factory.Sequence(lambda n: 'foo%d' % n)
 
 
 class NonIntegerPkFactory(SQLAlchemyModelFactory):
     FACTORY_FOR = models.NonIntegerPk
     FACTORY_SESSION = models.session
 
-    id = factory.Sequence(lambda n: u'foo%d' % n)
+    id = factory.Sequence(lambda n: 'foo%d' % n)
 
 
 @unittest.skipIf(sqlalchemy is None, "SQLalchemy not installed.")
@@ -70,32 +70,32 @@ class SQLAlchemyPkSequenceTestCase(unittest.TestCase):
 
     def test_pk_first(self):
         std = StandardFactory.build()
-        self.assertEqual(u'foo1', std.foo)
+        self.assertEqual('foo1', std.foo)
 
     def test_pk_many(self):
         std1 = StandardFactory.build()
         std2 = StandardFactory.build()
-        self.assertEqual(u'foo1', std1.foo)
-        self.assertEqual(u'foo2', std2.foo)
+        self.assertEqual('foo1', std1.foo)
+        self.assertEqual('foo2', std2.foo)
 
     def test_pk_creation(self):
         std1 = StandardFactory.create()
-        self.assertEqual(u'foo1', std1.foo)
+        self.assertEqual('foo1', std1.foo)
         self.assertEqual(1, std1.id)
 
         StandardFactory.reset_sequence()
         std2 = StandardFactory.create()
-        self.assertEqual(u'foo2', std2.foo)
+        self.assertEqual('foo2', std2.foo)
         self.assertEqual(2, std2.id)
 
     def test_pk_force_value(self):
         std1 = StandardFactory.create(id=10)
-        self.assertEqual(u'foo1', std1.foo)  # sequence was set before pk
+        self.assertEqual('foo1', std1.foo)  # sequence was set before pk
         self.assertEqual(10, std1.id)
 
         StandardFactory.reset_sequence()
         std2 = StandardFactory.create()
-        self.assertEqual(u'foo11', std2.foo)
+        self.assertEqual('foo11', std2.foo)
         self.assertEqual(11, std2.id)
 
 
@@ -108,27 +108,27 @@ class SQLAlchemyNonIntegerPkTestCase(unittest.TestCase):
 
     def test_first(self):
         nonint = NonIntegerPkFactory.build()
-        self.assertEqual(u'foo1', nonint.id)
+        self.assertEqual('foo1', nonint.id)
 
     def test_many(self):
         nonint1 = NonIntegerPkFactory.build()
         nonint2 = NonIntegerPkFactory.build()
 
-        self.assertEqual(u'foo1', nonint1.id)
-        self.assertEqual(u'foo2', nonint2.id)
+        self.assertEqual('foo1', nonint1.id)
+        self.assertEqual('foo2', nonint2.id)
 
     def test_creation(self):
         nonint1 = NonIntegerPkFactory.create()
-        self.assertEqual(u'foo1', nonint1.id)
+        self.assertEqual('foo1', nonint1.id)
 
         NonIntegerPkFactory.reset_sequence()
         nonint2 = NonIntegerPkFactory.build()
-        self.assertEqual(u'foo1', nonint2.id)
+        self.assertEqual('foo1', nonint2.id)
 
     def test_force_pk(self):
         nonint1 = NonIntegerPkFactory.create(id='foo10')
-        self.assertEqual(u'foo10', nonint1.id)
+        self.assertEqual('foo10', nonint1.id)
 
         NonIntegerPkFactory.reset_sequence()
         nonint2 = NonIntegerPkFactory.create()
-        self.assertEqual(u'foo1', nonint2.id)
+        self.assertEqual('foo1', nonint2.id)
