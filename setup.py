0e73c401d3a0aa0780bf428cc2f4cea142118ac2
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2015-04-21 11:33:08 +0200

    Declare Python3.4 support

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Testing",
         "Topic :: Software Development :: Libraries :: Python Modules"
