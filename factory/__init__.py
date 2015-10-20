3b3fed10ba95ef55cf057994922af55defd007ba
Raphaël Barrois <raphael.barrois@polytechnique.org>
2015-10-20 23:58:48 +0200

    Release v2.6.0

--- factory/__init__.py
+++ factory/__init__.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-__version__ = '2.5.2'
+__version__ = '2.6.0'
 __author__ = 'Raphaël Barrois <raphael.barrois+fboy@polytechnique.org>'
 
 
