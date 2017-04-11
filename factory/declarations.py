0bd4b61985e60fd53d89c3aac5c896f6606fe653
Raphaël Barrois <raphael.barrois@polytechnique.org>
2017-04-11 22:25:07 +0200

    Traits: only override when triggered.
    Introduce a ``factory.declarations.SKIP`` object, that is never passed
    to an object's constructor - otherwise, an un-activated Trait would try
    to pass 'None' as a value to the model constructor, overriding default
    values for keyword arguments.

--- factory/declarations.py
+++ factory/declarations.py
@@ -425,12 +425,19 @@ class List(SubFactory):
 # ==========
 
 
-class UNDEFINED(object):
-    pass
+class Skip(object):
+    def __bool__(self):
+        return False
+
+    # Py2 compatibility
+    __nonzero__ = __bool__
+
+
+SKIP = Skip()
 
 
 class Maybe(BaseDeclaration):
-    def __init__(self, decider, yes_declaration, no_declaration=None):
+    def __init__(self, decider, yes_declaration=SKIP, no_declaration=SKIP):
         self.decider = decider
         self.yes = yes_declaration
         self.no = no_declaration
@@ -505,7 +512,7 @@ class Trait(Parameter):
             overrides[maybe_field] = Maybe(
                 decider=field_name,
                 yes_declaration=new_value,
-                no_declaration=declarations.get(maybe_field, None),
+                no_declaration=declarations.get(maybe_field, SKIP),
             )
         return overrides
 
