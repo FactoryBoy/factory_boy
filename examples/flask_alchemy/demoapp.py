b40bec85439f8b45f338b848b118ee11aec52377
Raphaël Barrois <raphael.barrois@polytechnique.org>
2018-05-04 23:07:45 +0200

    examples: Adjust to Flask>=1.0 extension nanespace
    Previous versions of Flask installed extensions under `flask.ext`; that
    is no longer the case.

--- examples/flask_alchemy/demoapp.py
+++ examples/flask_alchemy/demoapp.py
@@ -2,7 +2,7 @@
 # Copyright: See the LICENSE file.
 
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
