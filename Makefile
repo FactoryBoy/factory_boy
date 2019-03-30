FactoryBoy/factory_boy

commit 239bdeac12fba5b0abb3599be3165d91343923ef

Simplify test discovery
Use `unittest` test discovery feature instead of making star-imports in
tests/__init__.py. It avoids explicitly listing all test cases and
allows linting tools like flake8 to identify unknown names. More reasons
not to use star imports can be found on this stack overflow answer
https://stackoverflow.com/a/2386740.

Unittest discover feature is described in
https://docs.python.org/3.7/library/unittest.html#unittest-test-discovery.

Makefile
@@ -50,7 +50,7 @@ testall:
 
 # DOC: Run tests for the currently installed version
 test:
-	python -Wdefault -m unittest $(TESTS_DIR)
+	python -Wdefault -m unittest
 
 # DOC: Test the examples
 example-test:

setup.py
@@ -73,6 +73,6 @@ setup(
         "Topic :: Software Development :: Testing",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    test_suite='tests',
+    test_suite='',
     test_loader=test_loader,
 )

tests/__init__.py
@@ -1,16 +1,0 @@
-# -*- coding: utf-8 -*-
-# Copyright: See the LICENSE file.
-
-# factory.django needs a configured Django.
-from .test_django import *
-
-from .test_base import *
-from .test_declarations import *
-from .test_docs_internals import *
-from .test_faker import *
-from .test_fuzzy import *
-from .test_helpers import *
-from .test_using import *
-from .test_utils import *
-from .test_alchemy import *
-from .test_mongoengine import *

commit c7078814437ddaef78c35cce9aa8f650612ea33d

Restore tests for Python 2.7
Regression introduced in 239bdeac12fba5b0abb3599be3165d91343923ef.

Makefile
@@ -51,7 +51,7 @@ testall:
 
 # DOC: Run tests for the currently installed version
 test:
-	python -Wdefault -m unittest
+	python -Wdefault -m unittest discover
 
 # DOC: Test the examples
 example-test: