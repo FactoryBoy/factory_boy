fdd965127ad8055a9c187caf61c0d1ac0f4e3103
Jon Dufresne <jon.dufresne@gmail.com>
2020-02-11 03:46:37 -0800

    Fix typo: GenericForenikeyConfig → GenericForeignKeyConfig

--- examples/django_demo/generic_foreignkey/apps.py
+++ examples/django_demo/generic_foreignkey/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class GenericForenikeyConfig(AppConfig):
+class GenericForeignKeyConfig(AppConfig):
     name = 'generic_foreignkey'
