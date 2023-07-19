f0859075b048b85c8ed44c051f18070cbd20d03d
Raphaël Barrois <raphael.barrois@spacinov.com>
2023-07-19 15:27:41 +0200

    Fix test_version
    That test used to hardcode the current dev version; this conflicts with
    the goal of storing said version only in setup.cfg.
    
    Further issues might be caused by `pip install -e .[dev]`; in that case,
    the check will have to be further loosened.

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,10 +1,19 @@
 # Copyright: See the LICENSE file.
 
+import pathlib
 import unittest
 
 import factory
 
+SETUP_CFG_VERSION_PREFIX = "version ="
 
 class VersionTestCase(unittest.TestCase):
+    def get_setupcfg_version(self):
+        setup_cfg_path = pathlib.Path(__file__).parent.parent / "setup.cfg"
+        with setup_cfg_path.open("r") as f:
+            for line in f:
+                if line.startswith(SETUP_CFG_VERSION_PREFIX):
+                    return line[len(SETUP_CFG_VERSION_PREFIX):].strip()
+
     def test_version(self):
-        self.assertEqual(factory.__version__, "3.2.1.dev0")
+        self.assertEqual(factory.__version__, self.get_setupcfg_version())
