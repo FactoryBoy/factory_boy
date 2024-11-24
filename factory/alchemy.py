356f8a3a0271246296086178b22419e4bff4c35a
Piotr Szyma <pszyma@opera.com>
2024-11-24 20:54:41 +0100

    MISC: add generic parameter to SQLAlchemyModelFactory

--- factory/alchemy.py
+++ factory/alchemy.py
@@ -43,7 +43,7 @@ class SQLAlchemyOptions(base.FactoryOptions):
         ]
 
 
-class SQLAlchemyModelFactory(base.Factory):
+class SQLAlchemyModelFactory[T](base.Factory[T]):
     """Factory for SQLAlchemy models. """
 
     _options_class = SQLAlchemyOptions
