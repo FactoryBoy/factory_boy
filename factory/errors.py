bacdd15d42962dbabdeb43807d617d2cdb309b3d
Noam Kushinsky <noamkush@gmail.com>
2025-03-03 13:47:40 +0200

    Evaluate error string lazily for performance.

--- factory/errors.py
+++ factory/errors.py
@@ -27,3 +27,17 @@ class InvalidDeclarationError(FactoryError):
     This means that the user declared 'foo__bar' without adding a declaration
     at 'foo'.
     """
+
+
+class LazyAttributeError(AttributeError):
+    """ A lazily evaluated version of AttributeError.
+
+    To be used when generating the error message is expensive.
+    """
+    def __init__(self, message, *args):
+        super().__init__()
+        self._message = message
+        self._args = args
+
+    def __str__(self):
+        return self._message % self._args
